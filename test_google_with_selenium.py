e229bc380ce589677a066d2c42d3c313d8a61259
Your Name <rami19shehk@gmail.com>
2022-07-03 23:39:41 +0300

    update

--- test_google_with_selenium.py
+++ test_google_with_selenium.py
@@ -6,6 +6,7 @@ import pytest
 
 from selenium import webdriver
 from selenium.webdriver import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.options import Options as FireFoxOptions
 from selenium.webdriver.firefox.service import Service as FirefoxService
@@ -14,44 +15,49 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+
+
 @pytest.fixture()
 def driver():
-
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
     dc = {
-        "browserName": "firefox",
+        "browserName": "chrome",
         "platformName": "Windows 10"
     }
 
     # selenium grid standAlone
-    driver = webdriver.Remote("http://localhost:4444",dc)
-
+    #driver = webdriver.Remote("http://localhost:4444",dc,options=chrome_options)
+    driver = webdriver.Remote("http://localhost:4444", dc)
     yield driver
     driver.close()
 
 def test_google_page_title(driver):
     driver.get('https://www.google.com')
     title = driver.title
+    driver.save_screenshot("screenshots2.png")
     assert title == str.title("google")
 
-def test_youtube_page_title(driver):
-    driver.get('https://www.youtube.com')
-    title =str(driver.title)
-    assert "youtube" in title.lower()
 
-def test_addition_of_2_and_5_simple(driver):
-    driver.get('https://www.google.com')
-    # css_selector, xpath
-    search_field = driver.find_element(By.NAME, "q")
-    search_field.click()
-    search_field.send_keys("2 + 5")
-    search_field.send_keys(Keys.ENTER)
-
-    ans_selector = "#cwos"
-
-    # WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR,ans_selector)))
-    # actual_field = driver.find_element(By.CSS_SELECTOR,ans_selector)
-
-    actual_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ans_selector)))
-
-    assert actual_field.text == str(7)
-
+# def test_youtube_page_title(driver):
+#     driver.get('https://www.youtube.com')
+#     title =str(driver.title)
+#     assert "youtube" in title.lower()
+#
+# def test_addition_of_2_and_5_simple(driver):
+#     driver.get('https://www.google.com')
+#     # css_selector, xpath
+#     search_field = driver.find_element(By.NAME, "q")
+#     search_field.click()
+#     search_field.send_keys("2 + 5")
+#     search_field.send_keys(Keys.ENTER)
+#
+#     ans_selector = "#cwos"
+#
+#     # WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR,ans_selector)))
+#     # actual_field = driver.find_element(By.CSS_SELECTOR,ans_selector)
+#
+#     actual_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ans_selector)))
+#
+#     assert actual_field.text == str(7)
+#
